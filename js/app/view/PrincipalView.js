93903a8337e55b7ffcf3bce89c29770b27ae5631
markiing <mvcartagenes@gmail.com>
2017-03-27 21:05:45 -0300

    Mudanças de estilização

--- js/app/view/PrincipalView.js
+++ js/app/view/PrincipalView.js
@@ -9,7 +9,7 @@ class PrincipalView extends View{
     _template(model){
         return  `<div>
                     <div class="projectConfiguration">
-                        <p>${model._name}</p>
+                        <p>Projeto: ${model._name}</p>
                     </div>
                     ${this._generateClasses(model._classes)}
                 </div>`
@@ -23,6 +23,7 @@ class PrincipalView extends View{
                         <span class="spanClass"> | Expand </span>
                         <span class="spanClass"> Class Info </span>
                         <p>${c._name} - Classe</p>
+                        <hr/>
                         ${exibirConstrutor?this._generateConstructors(c):''}
                         ${exibirMetodo?this._generateMethods(c):''}
                      </div>`
@@ -43,11 +44,14 @@ class PrincipalView extends View{
         let exibirParametros = false;
         return `${classe._methods.map(c=>
                     `<div class="methods">
-                        <div class="typeRequest">/${c._typeRequest}</div>
-                        <div class="method">${c._url}
-                        <span class="methodSpan">${c._description}</span>
+                        <div class="row">
+                            <div class="col-md-1">
+                                <div class="${this._getClassParameterByType(c._typeRequest)}">${c._typeRequest}</div>
+                            </div>
+                            <div class="col-md-10 col-xs-10 col-ls-10 method ${this._getClassMethodByType(c._typeRequest)}">
+                                <p>${c._url}<span class="methodSpan">${c._description}</span></p>
+                            </div>
                         </div>
-
                         ${exibirParametros?this._generateParameters(c):''}
                      </div>`
                 ).join('')}`
@@ -60,5 +64,30 @@ class PrincipalView extends View{
                      </div>`
                 ).join('')}`
     }
+
+    _getClassParameterByType(type){
+        if(type === "GET"){
+            return "typeRequest classGET";
+        }else if(type === "POST"){
+            return "typeRequest classPOST";
+        }else if(type === "PUT"){
+            return "typeRequest classPUT";
+        }else if(type==="DELETE"){
+            return "typeRequest classDELETE";
+        }
+    }
+
+    _getClassMethodByType(type){
+        if(type === "GET"){
+            return "get";
+        }else if(type === "POST"){
+            return "post";
+        }else if(type === "PUT"){
+            return "put";
+        }else if(type==="DELETE"){
+            return "delete";
+        }
+    }
+
 
 }
