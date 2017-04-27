05d5cd436a38d54191cd9b2f0cf2abf1847cbab0
Marcus Cartágenes <mvcartagenes@gmail.com>
2017-04-26 23:41:02 -0300

    Subindo gitignore

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -7,7 +7,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var app_component_1 = require("./app.component");
+var forms_1 = require("@angular/forms");
+var login_component_1 = require("./components/login/login.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -15,9 +16,9 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-        declarations: [app_component_1.AppComponent],
-        bootstrap: [app_component_1.AppComponent]
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
+        declarations: [login_component_1.LoginComponent],
+        bootstrap: [login_component_1.LoginComponent]
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule,FormsModule ],
+  declarations: [ LoginComponent],
+  bootstrap:    [ LoginComponent]
 })
 export class AppModule { }
